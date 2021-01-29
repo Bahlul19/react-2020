93693f7684d9ad9d3b592a551b6d3c99ab098808
Bahlul19 <bahlul.siddiquee@sjinnovation.com>
2021-01-29 11:24:32 +0600

    Learning about the dynamic content for the React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    // this.state = {
+    //   string : 'Hello Sylhet'
+    // };
+
+    this.state = {
+      monsters : [
+        {
+          name: 'ABCD',
+          id : '1'
+        },
+
+        {
+          name: 'SJI',
+          id: '2'
+        },
+
+        {
+          name: 'SYLHET',
+          id: '3'
+        }
+      ]
+
+    // monstersNew : [
+    //   {
+    //     color : 'RED',
+    //     id : '23'
+    //   },
+
+    //   {
+    //     color : 'Green',
+    //     id : '33'
+    //   }
+    // ]
+    
+  }
 }
 
+  render(){
+    return (
+      <div className="App">
+        
+        { 
+        this.state.monsters.map(
+          monsters =>
+           <h1 key={ monsters.id }>
+             { monsters.name }
+           </h1>
+        )
+
+        // this.state.monstersNew.map(
+        //   monstersNew =>
+        //   <h3 key={ monstersNew.id }>
+        //   { monstersNew.name }
+        //   </h3>
+        // )
+        }
+
+      </div>
+    );
+  }
+}
 export default App;
