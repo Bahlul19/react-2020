2e0128ba853287ca81dc493a7a83cde58b7f27e1
Bahlul19 <bahlul.siddiquee@sjinnovation.com>
2021-02-03 13:38:13 +0600

    added the functionality for search field state for monster search functionality

--- src/App.js
+++ src/App.js
@@ -24,6 +24,18 @@ componentDidMount(){
   render(){
     return (
       <div className="App">
+        {/* <input type="search" placeholder="Search Monster" 
+        onChange={ e => console.log(e.target.value)}></input> */}
+         {/* <input type="search" placeholder="Search Monster" 
+        onChange = { 
+          e => { this.setState({ searchField: e.target.value });
+          console.log(this.state)
+        }} /> */}
+        <input type="search" placeholder="Search Monster" onChange= {
+          e => { 
+          this.setState ( { searchField : e.target.value }, 
+          () => console.log(this.state) );
+           }} />
         <CardList monstersNew={this.state.monstersNew} />
       </div>
     );
